0ed8a3ef6a57889591805e7023acef819ee00858
sadrakss <sadrakss@pop-os.localdomain>
2020-02-20 15:27:29 -0300

    base syntax  and comment

---
+++ 002/js/scripts.js
@@ -0,0 +1,7 @@
+function teste() {
+    let resultado = 10+209
+
+    document
+    .getElementById('info')
+    .innerHTML = 'O Resultado é : ' + resultado
+}
---
+++ 003/js/scripts.js
@@ -0,0 +1,25 @@
+var nome = 'joão', apelido = 'pedro', pais = portugal, idade = 12
+var idade = 10
+
+
+var animal
+animal = 'lion'
+
+var play = true
+function teste() {
+    var stop = false
+
+    // arrays
+    var nomes = [
+        'sadrak', 'pedro', 'maria'
+    ]
+
+    // objects
+    var car = {
+        brand: 'Mercedes',
+        year: 2019,
+        color: 'grey'
+    }
+
+    document.getElementById('info').innerHTML = car.brand + ' ( ' + car.year + ' ) '
+}
