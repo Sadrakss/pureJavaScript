a73460f3ad736c7831838540e13caba607c8e7eb
sadrakss <sadrakss@pop-os.localdomain>
2020-02-22 12:06:31 -0300

    ready javaScript functions

---
+++ 008/js/scripts.js
@@ -0,0 +1,11 @@
+'use strict'
+function test() {
+
+    let text = 'sadrak silva sousa '
+
+    // document.getElementById('info').innerHTML = text.length
+    // document.getElementById('info').innerHTML = text.charAt(0)
+    // document.getElementById('info').innerHTML = text.includes('sadrak')
+    // document.getElementById('info').innerHTML = text.repeat(10)
+    document.getElementById('info').innerHTML = text.toUpperCase()
+}
